1d5000c817a884a7153bb8f0b15329f5d5b43031
evelynlizzethe <elriofrio1@utpl.edu.ec>
2024-01-04 17:40:58 -0500

    Taller11

--- ejercicios/Problemas/src/problemas/Problema1.java
+++ ejercicios/Problemas/src/problemas/Problema1.java
@@ -5,6 +5,7 @@
  */
 package problemas;
 
+import java.util.*;
 /**
  *
  * @author reroes
@@ -20,7 +21,16 @@ public class Problema1 {
     }
 
     public static void imprimirDatos(int[][] datos) {
-
+        String cadena = "";
+        for (int f = 0; f < datos.length; f++) {
+            for (int c = 0; c < datos[f].length; c++) {
+                if (datos[f][c] % 2 == 0) {
+                    cadena = String.format("%s%d ", cadena, datos[f][c]);
+                }
+            }
+        }
+        System.out.printf("Numeros pares de los arreglos son: \n"
+                + "%s\n", cadena);
     }
 
 }
--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.*;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,31 @@ public class Problema2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int num = 0;
+        System.out.println("Ingrese el numero del procedimiento que "
+                + "desea realizar:\n1. Area del Cuadrado\n2. Area del Triangulo\n"
+                + "3. Area del Rectangulo\n");
+        num = entrada.nextInt();
+
+        if (1 == 3) {
+            obtenerAreaCuadrado(lado);
+        } else if (1 == 3) {
+            obtenerAreaTriangulo(altura, base);
+        } else if (1 == 3) {
+            obtenerAreaRectangulo(altura, base);
+        }
     }
-    
+
+    public static void obtenerAreaCuadrado(int lado) {
+        Scanner entrada = new Scanner(System.in);
+        int lado = 0;
+        int total = 0;
+        System.out.println("Ingrese un valor");
+        
+    }
+
+}
+
 }
